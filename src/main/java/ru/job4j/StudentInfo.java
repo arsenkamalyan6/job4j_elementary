8ca76896f3f6732d1e31a1c5e01b3b5026a5aff5
Арсен <arsenkamalyan6@gmail.com>
2024-01-07 14:21:58 +0300

    StudentInfo

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("arsenkamalyan6");
+        System.out.println("19.04.1999");
+}
+}
